be80b78873e29ab37dada56b93e46f8e3bf3ded3
Edwar Plata <edwarplata@gmail.com>
2021-01-15 00:05:05 -0500

    Added blur on shower

--- client/viewer/main.js
+++ client/viewer/main.js
@@ -2,14 +2,18 @@ $(document).ready(() => {
     const socket = io('/viewer');
 
     socket.on('shower', (data) => {
+        $('.banner').addClass('blur');
+        console.log($('.banner'))
         $(`.${data.nombre}`).toggleClass('hide show');
         setTimeout(() => {
             $(`.${data.nombre}`).toggleClass('hide show');
         }, 5000);
+        setTimeout(() => {
+            $('.banner').removeClass('blur');
+        }, 10000);
     });
 
     socket.on('banner', (checked) => {
-        console.log(checked);
         if (!checked && $('.banner').hasClass('show')){
             $('.banner').removeClass('show');
             $('.banner').addClass('hide');
