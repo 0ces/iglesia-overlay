f7661dc568c338b75de24a5742327171d1f3d646
Edwar Plata <edwarplata@gmail.com>
2021-01-16 00:13:25 -0500

    Revert "Pantalla de inicio"
    This reverts commit fc837b5d43f1ba964c24b954660a8f4af4773a95.

--- client/viewer/main.js
+++ client/viewer/main.js
@@ -1,59 +1,15 @@
-function pad (str, max) {
-  str = str.toString();
-  return str.length < max ? pad("0" + str, max) : str;
-}
-
-class Timer {
-    constructor(segundos, elemento, callback){
-        this.segundos = segundos;
-        this.elemento = elemento;
-        this.callback = callback;
-        if (typeof this.elemento !== 'undefined'){
-            this.interval = setInterval(() => {
-                this.reducirElemento();
-            }, 1000);
-        } else {
-            this.interval = setInterval(() => {
-                this.reducir()
-            },1000);
-        }
-    }
-
-    reducirElemento(){
-        this.reducir();
-        $(this.elemento).text(this.getString());
-    }
-
-    reducir(){
-        this.segundos--;
-        if(this.segundos === 0){
-            this.stop();
-        }
-    }
-
-    getString(){
-        let minutos = Math.floor(this.segundos/60);
-        let segundos = this.segundos%60;
-        return `${pad(minutos,1)}:${pad(segundos,2)}`
-    }
-
-    stop(){
-        clearInterval(this.interval);
-    }
-}
-
 $(document).ready(() => {
     const socket = io('/viewer');
 
     socket.on('shower', (data) => {
-        $('.banner').addClass('blur-ani');
+        $('.banner').addClass('blur');
         console.log($('.banner'))
         $(`.${data.nombre}`).toggleClass('hide show');
         setTimeout(() => {
             $(`.${data.nombre}`).toggleClass('hide show');
         }, 5000);
         setTimeout(() => {
-            $('.banner').removeClass('blur-ani');
+            $('.banner').removeClass('blur');
         }, 10000);
     });
 
@@ -68,8 +24,6 @@ $(document).ready(() => {
         }
 
     });
-
-    new Timer(5*60,'.timer',() => {});
 });
 
 
