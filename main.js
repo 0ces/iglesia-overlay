3c0fe54d6638970c896a1b43a7f7e40ddcdc1d90
Edwar Plata <edwarplata@gmail.com>
2021-01-21 21:13:15 -0500

    Major update: direct download static from github

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ const path = require('path');
 const io = require('socket.io')(server1);
 const admin = io.of('/admin');
 const viewer = io.of('/viewer');
+const repo = '0ces/iglesia-overlay';
+const https = require('https');
+const tree = require('github-trees');
+const get = require('get-file');
 
 // const server2 = http.createServer((req, res) => {
 //     let stream = fs.createReadStream(path.join('.', req.url));
@@ -19,108 +23,142 @@ const viewer = io.of('/viewer');
 //     console.log('Listening for files requests on *:3001');
 // });
 
-app.use(express.static(__dirname + "/client"));
-app.use(express.static(__dirname + "/client/viewer"));
-fs.mkdir(path.join('.', '/videos'), { recursive: true }, (err) => {
-    if (err) throw err;
-});
+function downloadFile(repo, path) {
+    // console.log(`${repo}/${path}`);
+    get(repo, path, (error, response) => {
+        if (error) return console.error(error);
+        let file = fs.createWriteStream(path);
+        response.pipe(file);
+    });
+}
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/viewer/index.html');
-});
+async function getFilesFromRepo(repo){
+    let treeFromRepo = await tree(repo, {recursive:true}).then(res => res.tree);
+    treeFromRepo.forEach((file, i) => {
+        if(file.path.startsWith('client')){
+            if (file.type === 'tree'){
+                console.log(`Creando carpeta ${file.path}`);
+                fs.mkdir(file.path, (error) => {
+                    if (error) return console.error(error);
+                });
+            }
+            if (file.type === 'blob'){
+                console.log(`Descargando archivo ${file.path}`);
+                downloadFile(repo, file.path);
+            }
+        }
+    });
+}
 
-app.get('/viewer', (req, res) => {
-    res.sendFile(__dirname + '/client/viewer/index.html');
-});
+if (process.argv.indexOf('--no-git') >= 0){
+    main();
+} else {
+    getFilesFromRepo(repo).then(() => {
+        console.log('Termino!');
+        main();
+    });
+}
 
-app.get('/admin', (req, res) => {
-    res.sendFile(__dirname + '/client/admin/index.html')
-});
-
-admin.on('connection', (socket) => {
-    let address = socket.handshake.address.replace('::ffff:', '');
-    console.log(`Se ha conectado ${address} a /admin`)
-
-    socket.on('shower', (data) => {
-        socket.broadcast.emit('shower', data);
-        viewer.emit('shower', data);
+function main() {
+    app.use(express.static("client"));
+    app.use(express.static("client/viewer"));
+    app.get('/', (req, res) => {
+        res.sendFile('client/viewer/index.html');
     });
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected from /admin');
+    app.get('/viewer', (req, res) => {
+        res.sendFile('client/viewer/index.html');
     });
 
-    socket.on('banner', (checked) => {
-        socket.broadcast.emit('banner', checked);
-        viewer.emit('banner', checked);
+    app.get('/admin', (req, res) => {
+        res.sendFile('client/admin/index.html')
     });
 
-    socket.on('changer', (data) =>  {
-        socket.broadcast.emit('changer', data);
-        viewer.emit('changer', data);
+    admin.on('connection', (socket) => {
+        let address = socket.handshake.address.replace('::ffff:', '');
+        console.log(`Se ha conectado ${address} a /admin`)
+
+        socket.on('shower', (data) => {
+            socket.broadcast.emit('shower', data);
+            viewer.emit('shower', data);
+        });
+
+        socket.on('disconnect', () => {
+            console.log('User disconnected from /admin');
+        });
+
+        socket.on('banner', (checked) => {
+            socket.broadcast.emit('banner', checked);
+            viewer.emit('banner', checked);
+        });
+
+        socket.on('changer', (data) =>  {
+            socket.broadcast.emit('changer', data);
+            viewer.emit('changer', data);
+        });
+
+        socket.on('scene', seleccionado => {
+            socket.broadcast.emit('scene', seleccionado);
+            viewer.emit('scene', seleccionado);
+        });
+
+        socket.on('timer', (data) => {
+            viewer.emit('timer', data);
+        });
+
+        socket.on('logo-pos', selected => {
+            socket.broadcast.emit('logo-pos', selected);
+            viewer.emit('logo-pos', selected);
+        });
+
+        socket.on('youtube-source', (data) => {
+            viewer.emit('youtube-source', data);
+        });
+
+        socket.on('get-youtube-current-time', () => {
+            viewer.emit('get-youtube-current-time');
+        });
+
+        socket.on('youtube-play', () => {
+            viewer.emit('youtube-play');
+        });
+
+        socket.on('youtube-pause', () => {
+            viewer.emit('youtube-pause');
+        });
+
+        socket.on('youtube-seek', (percentage) => {
+            socket.broadcast.emit('youtube-seek', percentage);
+            viewer.emit('youtube-seek', percentage);
+        });
+    })
+
+    viewer.on('connection', (socket) => {
+        let address = socket.handshake.address.replace('::ffff:', '');
+        console.log(`Se ha conectado ${address} a /viewer`)
+
+        socket.on('disconnect', () => {
+            console.log('User disconnected from /viewer');
+        });
+
+        socket.on('youtube-state-change', () => {
+            admin.emit('youtube-state-change');
+        });
+
+        socket.on('youtube-data', (data) => {
+            admin.emit('youtube-data', data);
+        });
+
+        socket.on('youtube-current-time', (data) => {
+            admin.emit('youtube-current-time', data);
+        });
+
+        socket.on('youtube-playing', () => {
+            admin.emit('youtube-playing');
+        });
     });
 
-    socket.on('scene', seleccionado => {
-        socket.broadcast.emit('scene', seleccionado);
-        viewer.emit('scene', seleccionado);
+    server1.listen(3000, () => {
+        console.log('Listening on *:3000');
     });
-
-    socket.on('timer', (data) => {
-        viewer.emit('timer', data);
-    });
-
-    socket.on('logo-pos', selected => {
-        socket.broadcast.emit('logo-pos', selected);
-        viewer.emit('logo-pos', selected);
-    });
-
-    socket.on('youtube-source', (data) => {
-        viewer.emit('youtube-source', data);
-    });
-
-    socket.on('get-youtube-current-time', () => {
-        viewer.emit('get-youtube-current-time');
-    });
-
-    socket.on('youtube-play', () => {
-        viewer.emit('youtube-play');
-    });
-
-    socket.on('youtube-pause', () => {
-        viewer.emit('youtube-pause');
-    });
-
-    socket.on('youtube-seek', (percentage) => {
-        socket.broadcast.emit('youtube-seek', percentage);
-        viewer.emit('youtube-seek', percentage);
-    });
-})
-
-viewer.on('connection', (socket) => {
-    let address = socket.handshake.address.replace('::ffff:', '');
-    console.log(`Se ha conectado ${address} a /viewer`)
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected from /viewer');
-    });
-
-    socket.on('youtube-state-change', () => {
-        admin.emit('youtube-state-change');
-    });
-
-    socket.on('youtube-data', (data) => {
-        admin.emit('youtube-data', data);
-    });
-
-    socket.on('youtube-current-time', (data) => {
-        admin.emit('youtube-current-time', data);
-    });
-
-    socket.on('youtube-playing', () => {
-        admin.emit('youtube-playing');
-    });
-});
-
-server1.listen(3000, () => {
-    console.log('Listening on *:3000');
-});
+}
